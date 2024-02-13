32e98b226661c708c60392a1c178d130a1718211
SchokiCoder <afschoknecht@tutanota.com>
2024-02-13 21:54:36 +0100

    add text style (wip)
    Need to hand theme over.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright (C) 2024  Andy Frank Schoknecht
 
+use iced::Color;
 use iced::{Element, Sandbox, Settings};
+use iced::font::*;
 use iced::time;
-use iced::widget::{text};
+use iced::theme::{self, Theme};
+use iced::widget::{column, container, text};
 use std::{io};
 use std::io::Read;
 
-/*#[derive(Debug, Clone, Copy)]
-pub enum Message {
-	Click
-}*/
-
 struct Twiggie {
-	//message: Message,
+	theme: Theme,
 	start: time::Instant,
 	uptime: time::Duration,
 }
@@ -23,28 +21,44 @@ impl Sandbox for Twiggie {
 
 	fn new() -> Twiggie
 	{
-		return Twiggie {
+		let palette = theme::Palette {
+			background: Color::from_rgb(0.24, 0.643, 0.565),
+			text: Color::from_rgb(0.212, 0.996, 0.8),
+			primary: Color::from_rgb(0.5, 0.5, 0.0),
+			success: Color::from_rgb(0.0, 1.0, 0.0),
+			danger: Color::from_rgb(1.0, 0.0, 0.0),
+		};
+
+		let ret = Twiggie {
+			theme: Theme::custom(palette),
 			start: time::Instant::now(),
 			uptime: time::Duration::from_nanos(0),
 		};
+
+		return ret;
 	}
 
 	fn view(&self) -> Element<Self::Message>
 	{
 		let intro = match self.uptime.as_secs() { 
 		0 | 1 => {
-			text("YOU ARE NOW").size(50)
+			text("YOU ARE NOW")
 		}
 		
 		_ => {
 			text("").size(0)
 		}};
 
-		intro.into()
+		let content = column![
+			self.theme,
+			intro
+		];
+
+		container(content)
 	}
 	
-	fn update(&mut self, msg: Self::Message)
-	{	
+	fn update(&mut self, _msg: Self::Message)
+	{
 		self.uptime = self.start.elapsed();
 	}
 	
@@ -56,6 +70,14 @@ impl Sandbox for Twiggie {
 
 fn main()
 {
+	let mut settings = Settings::default();
+	settings.antialiasing = false;
+	settings.default_font.stretch = Stretch::Normal;
+	settings.default_font.monospaced = true;
+	settings.default_text_size = 14.0;
+	settings.window.size = (320, 200);
+	let settings = settings;
+
 	let mut input: [u8; 1] = [0];
 	let mut stdin = io::stdin();
 
@@ -69,7 +91,12 @@ fn main()
 
 	match input[0] as char {
 	'y' => {
-		Twiggie::run(Settings::default());
+		match Twiggie::run(settings) {
+		Ok(_) => {}
+		
+		Err(e) => {
+			panic!("Could not run program: {}", e);
+		}}
 	}
 	
 	_ => {}
--- todo.md
+++ todo.md
@@ -1,5 +1,18 @@
+# wait for iced scaling
+
+- once iced adds scaling, scale the window to any logical displaying size
+  currently it should be super small, so that the crustiness is basically native,
+  see source video
+
+- only after that i can do actual visual fine tuning
+
+# basics
+
 + add cli
 + add window
 - add text style
+- add text logic for intro
+- add text logic for content
 - add graphics
 - add music ?
+- set version 0.69.0
